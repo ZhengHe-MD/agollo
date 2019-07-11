44a6f590a51cce721b0f319ed71cd50235683bbd
zhenghe3119 <zhenghe3119@ipalfish.com>
2019-07-11 21:05:52 +0800

    update longPollInterval and longPollTimeout

--- const.go
+++ const.go
@@ -10,8 +10,8 @@ const (
 
 	longPollInterval      = time.Second * 2
 	// NOTE: apollo will return 304 after 60 secs when querying config for a
-	//       unknown app, so timeout larger than 60 secs doesn't make sense
-	longPollTimeout       = time.Second * 5
+	//       unknown app, so longPollTimeout should be larger than 60 secs
+	longPollTimeout       = time.Second * 90
 	queryTimeout          = time.Second * 2
 	defaultNotificationID = -1
 )
